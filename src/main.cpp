32007940eaf1d81c12b71c88bc9aed73348dffe1
Shorts1999 <sjorssmit1999@gmail.com>
2022-03-04 20:52:47 +0100

    start a new main for ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -1,114 +1,34 @@
-/*
-   Multiple games for on Arexx Game Board
-   Author: Sjors Smit
-   Date: June 2nd 2020
-   AREXX ENGINEERING
-*/
+/**
+ * @file main.cpp
+ * 
+ * @author Sjors Smit
+ * @brief Firmware for the Arexx Game Board v2.0 using an ESP32
+ * @version 2.0
+ * @date 2022-02-21
+ */
+
 #include <Arduino.h>
-#include <Wire.h>
 #include <SPI.h>
-#include "math.h"
+#include <math.h>
 
-#include "Arexx_GameBoard.h"
-#include "Arexx_Tetris/Arexx_Tetris.h"
-#include "Arexx_Pong/Arexx_Pong.h"
-#include "Arexx_Clock/Arexx_Clock.h"
+#include <Arexx_GameBoard.h>
 
-/*
-   Backgrounds. These are shown during the menu
-*/
+//definitions for the main LED-matrix/display:
+#define MATRIX_PIN 6
+#define MATRIX_WIDTH 10      //the amount of horizontal pixels in a single panel
+#define MATRIX_HEIGHT 20     //the amount of vertical pixels in a single panel
+#define PANELTYPE  (NEO_MATRIX_BOTTOM + NEO_MATRIX_LEFT + NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG)
+#define TILEORIENTATION (NEO_TILE_TOP   + NEO_TILE_LEFT   + NEO_TILE_COLUMNS   + NEO_TILE_PROGRESSIVE)
+ArexxGameBoard gameBoard(MATRIX_PIN, MATRIX_WIDTH, MATRIX_HEIGHT, PANELTYPE, NEO_GRB + NEO_KHZ800, 6);
 
-
-void setup() {
-  Serial.begin(9600); //Initialize the Serial port
-  delay(200); //give modules some time to boot up before initialization
-
-  for (int i = 0; i < TILEHEIGHT + 4; i++) { //Setup the Board array with the empty field, bars on the side to indicate the walls for collision detection
-    Board[i] =  0x8000 | (0x8000 >> (TILEWIDTH + 1));
-  }
-  //Also add one full row at the bottom (will be one pixel underneath the actual display)
-  Board[TILEHEIGHT + 4] = 0xffff;
-
-  matrix.begin(); //Initializing the NeoPixel display
-  matrix.clear();
-  matrix.show();
-  matrix.setBrightness(180); //set Brightness of the main display
-
-  //Set up controller
-  while (!ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, false, false)) {
-  }
-
-  scorePanel.setIntensity(2);   //set Brightness of the score display
-  for (int i = 0; i < 8; i++) { //Rotate all
-    scorePanel.setRotation(i, 1);
-  }
-
-  scorePanel.print("AREXX");
-  scorePanel.write();
-
-  pinMode(INT_PIN, INPUT_PULLUP);
-  RTC.start(); //Initialize the RTC module
-
-  oldTime = millis(); //set the timer to the current time
-  newTime = oldTime;
-
-  matrix.clear(); //empty the display
-  randomSeed(analogRead(A0)); //Set a place for the random number generator using a floating pin
-  delay(10);
-  shape = random(0, 7); //get a "fake" random number to avoid starting at 0 every time, since random seemed to always boot at 0, even after setting a seed
+void setup(){
+    Serial.begin(115200);
+    gameBoard.begin();
+    gameBoard.setBrightness(10);
+    gameBoard.show();
 
 }
 
+void loop(){
 
-void loop() { //main menu / game select screen.
-  delay(5);
-  ps2x.read_gamepad(); //Added delay before and after reading gamepad to improve stability.
-  delay(5);
-  if (ps2x.ButtonPressed(PSB_CROSS)) { //Pressing the Cross button cycles the game selection forwards
-    if (pendingSelect >= gameAmount) {
-      pendingSelect = 0;
-    }
-    else pendingSelect++;
-  }
-  if (ps2x.ButtonPressed(PSB_CIRCLE)) { //Pressing the Circle button cycles the game selection backwards
-    if (pendingSelect == 0) {
-      pendingSelect = gameAmount;
-    }
-    else pendingSelect--;
-  }
-  switch (pendingSelect) {
-    case 0:
-      matrix.setCursor(3, 3);
-      matrix.clear();
-      matrix.print(pendingSelect);
-      clockBackground();
-      matrix.show();
-      if (ps2x.ButtonPressed(PSB_START)) {
-        matrix.clear();
-        displayClock();
-      }
-      break;
-    case 1:
-      matrix.setCursor(3, 3);
-      matrix.clear();
-      matrix.print(pendingSelect);
-      tetrisBackground();
-      matrix.show();
-      if (ps2x.ButtonPressed(PSB_START)) { //pressing the start button launches the currently selected game (TETRIS)
-        matrix.clear();
-        mainTetris();
-      }
-      break;
-    case 2:
-      matrix.setCursor(3, 3);
-      matrix.clear();
-      matrix.print(pendingSelect);
-      pongBackground();
-      matrix.show();
-      if (ps2x.ButtonPressed(PSB_START)) { //Pressing the start button launches the currently selected game (PONG)
-        matrix.clear();
-        mainPong();
-      }
-      break;
-  }
 }
